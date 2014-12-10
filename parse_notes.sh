24577b47e552a2d7c0fa952b41381bc6078d3e9d
Magnus Berendes <magnus.berendes@gmx.net>
2014-12-10 03:10:46 +0100

    parse_notes.sh

---
+++ parse_notes.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "help" ]
+then 
+	echo "usage: ${0} <file_to_parse>";
+	echo "In file_to_parse:"
+	echo "	Use \"%!STRING\" inside of /begin{frame} and /end{frame} environment, STRING will then be written into a valid xml-file, that can be read by open-pdf-presenter"
+	exit;
+fi
+echo "Parsing notes to notes.xml"
+touch notes.xml;
+echo "<notes>" > notes.xml;
+i=1;
+while read line;
+do 
+	s1=${line:0:2};
+
+	if [ "${line:0:12}" = "begin{frame}" ]
+	then
+		echo "begin frame"
+		echo "<note number=\"${i}\">" >> notes.xml;
+	elif [ "${line}" = "end{frame}" ]
+	then
+		echo "end frame"
+		echo "</note>" >> notes.xml;
+		i=$((i + 1));
+	elif [ "${s1}" = "%!" ]
+	then
+		echo "${line:2} to notes.xml";
+		echo ${line:2} >> notes.xml;
+	fi
+done  < $1
+echo "</notes>" >> notes.xml;
